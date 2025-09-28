34c9e3b184703aabd5b2bfb9be6bc0f0aa5619bd
RyoyaGM <gakumonryoya@gmail.com>
2025-09-29 08:10:14 +0900

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>思索の地図</h1>
+      <p>拝光教の神学者たちとその思想</p>
+    </header>
+  );
+}
+
+export default Header;
